91501c93f3c34ff7e0851737d4a0b5746ddcbc63
pawannn <pawankalyan1614@gmail.com>
2026-02-19 00:47:48 +0530

    added concurrency expermiments

---
+++ go-concurrency/fanout-fanin.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"sync"
+)
+
+func RepeatFunc[T any, K any](done <-chan T, fn func() K) <-chan K {
+	generateChan := make(chan K)
+	go func() {
+		defer close(generateChan)
+		for {
+			select {
+			case <-done:
+				return
+			case generateChan <- fn():
+			}
+		}
+	}()
+
+	return generateChan
+}
+
+func PrimeFinder(done <-chan int, randNumChan <-chan int) <-chan int {
+	isPrime := func(num int) bool {
+		if num < 2 {
+			return false
+		}
+		for i := 2; i*i < num; i++ {
+			if num%i == 0 {
+				return false
+			}
+		}
+		return true
+	}
+
+	primeChannel := make(chan int)
+	go func() {
+		defer close(primeChannel)
+		for {
+			select {
+			case <-done:
+				return
+			case num, ok := <-randNumChan:
+				if !ok {
+					return
+				}
+				if isPrime(num) {
+					primeChannel <- num
+				}
+			}
+		}
+	}()
+
+	return primeChannel
+}
+
+func Take[T any, K any](done <-chan T, in <-chan K, limit int) <-chan K {
+	takeChan := make(chan K)
+	go func() {
+		defer close(takeChan)
+		for range limit {
+			select {
+			case <-done:
+				return
+			case takeChan <- <-in:
+			}
+		}
+	}()
+
+	return takeChan
+}
+
+func FanIn[T any, K any](done <-chan T, inChans ...<-chan K) <-chan K {
+	stream := make(chan K)
+	var wg sync.WaitGroup
+
+	readChannel := func(in <-chan K) {
+		wg.Go(func() {
+			for {
+				select {
+				case <-done:
+					return
+				case stream <- <-in:
+				}
+			}
+		})
+	}
+
+	for _, ch := range inChans {
+		go readChannel(ch)
+	}
+
+	go func() {
+		wg.Wait()
+		close(stream)
+	}()
+
+	return stream
+}
+
+func StartFan() {
+	done := make(chan int)
+	randomGenerator := func() int { return rand.Intn(50000000) }
+
+	randomNumChan := RepeatFunc(done, randomGenerator)
+
+	cpuCount := runtime.NumCPU()
+
+	primeFinderChannels := make([]<-chan int, cpuCount)
+	for i := range cpuCount {
+		primeFinderChannels[i] = PrimeFinder(done, randomNumChan)
+	}
+
+	fanInChannel := FanIn(done, primeFinderChannels...)
+
+	for num := range Take(done, fanInChannel, 2) {
+		fmt.Println(num)
+	}
+}
---
+++ go-concurrency/worker_pool.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
+
+type Task struct {
+	id   int
+	name string
+}
+
+func (t *Task) ProcessTask() {
+	fmt.Println("completing task ID : ", t.id, " task name : ", t.name)
+	time.Sleep(time.Second * 2)
+}
+
+type workerPool struct {
+	tasks       []Task
+	concurrency int
+	taskChan    chan Task
+	wg          sync.WaitGroup
+}
+
+func (wp *workerPool) worker() {
+	for task := range wp.taskChan {
+		task.ProcessTask()
+		wp.wg.Done()
+	}
+}
+
+func (wp *workerPool) Run() {
+	wp.wg.Add(len(wp.tasks))
+	wp.taskChan = make(chan Task, len(wp.tasks))
+	for range wp.concurrency {
+		go wp.worker()
+	}
+
+	for _, task := range wp.tasks {
+		wp.taskChan <- task
+	}
+
+	wp.wg.Wait()
+	close(wp.taskChan)
+}
+
+func main() {
+	tasks := make([]Task, 20)
+	for i := range 20 {
+		tasks[i].id = i
+		tasks[i].name = "work" + fmt.Sprint(i)
+	}
+
+	wp := workerPool{
+		tasks:       tasks,
+		concurrency: runtime.NumCPU(),
+	}
+
+	wp.Run()
+}
