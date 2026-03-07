0926ace02ba07e5a545f6e10464ab130ce20eb05
pawannn <pawankalyan1614@gmail.com>
2026-03-07 23:43:12 +0530

    chore: added context example

---
+++ go-concurrency/context.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func worker(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Stopped :", ctx.Err())
+			return
+		default:
+			fmt.Println("Running")
+		}
+	}
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	go worker(ctx)
+
+	time.Sleep(time.Second * 1)
+	cancel()
+
+	time.Sleep(time.Second * 1)
+}
