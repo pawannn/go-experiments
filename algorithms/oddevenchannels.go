3c6546f48044a0b0534c6c8c683d62aa09cbff60
pawannn <pawankalyan1614@gmail.com>
2026-04-28 23:37:15 +0530

    added odd even with channels

---
+++ algorithms/oddevenchannels.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func printOdd(ch chan int, done chan bool) {
+	for i := 1; i <= 10; i += 2 {
+		<-ch
+		fmt.Println("Odd:", i)
+		ch <- 1
+	}
+	done <- true
+}
+
+func printEven(ch chan int, done chan bool) {
+	for i := 2; i <= 10; i += 2 {
+		<-ch
+		fmt.Println("Even:", i)
+		ch <- 1
+	}
+	done <- true
+}
+
+func main() {
+	ch := make(chan int)
+	done := make(chan bool)
+
+	go printOdd(ch, done)
+	go printEven(ch, done)
+
+	ch <- 1
+
+	<-done
+	<-done
+}
